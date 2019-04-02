a23133f658d4a64c7657cc8896db8cae90f0728a
Alex Dome <alex.dome.2022@student.lhschools.org>
2019-04-02 14:00:13 -0400

    Brought over into Intellij

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,12 @@
+package com.LickingHeights;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner keyboard;
+        keyboard = new Scanner(System.in);
+    }
+}
