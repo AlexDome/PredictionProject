576015575063f98ce35e9f3d8b8b32841e1a1f34
Alex Dome <alex.dome.2022@student.lhschools.org>
2019-04-22 14:19:11 -0400

    started equations

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -8,6 +8,9 @@ public class Main {
 
         Scanner keyboard;
         keyboard = new Scanner(System.in);
+        int userMonth;
+        int userDay;
+
 
         System.out.println("Monday's child is fair of face.");
         System.out.println("Tuesday's child is full of grace.");
@@ -17,8 +20,14 @@ public class Main {
         System.out.println("Saturday's child works hard for a living.");
         System.out.println("But the child born on the Sabbath Day,");
         System.out.println("Is fair and wise and good in every way.");
-
+        System.out.println();
         System.out.println("Let's see what day of the week you were born on.");
         System.out.println("What month were you born in? Please enter as a number 1-12");
+        userMonth = keyboard.nextInt();
+
+        System.out.println();
+        System.out.println("Can you give me the day you were born on? Please give it to me as a number");
+        userDay = keyboard.nextInt();
+        System.out.println("Thank you very much for giving me this information, I will ")
     }
 }
